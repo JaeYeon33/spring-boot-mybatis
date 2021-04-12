56c6082bbc362fb089792be75029e559730350f7
JaeYeon33 <cjyeon1022@gmail.com>
2021-04-12 23:57:30 +0900

    2021-04-12 MySQL + MyBatis 연동하여 RESTfulAPI에 붙이기

--- start/src/main/java/com/mystart/start/controller/UserProfileController.java
+++ start/src/main/java/com/mystart/start/controller/UserProfileController.java
@@ -1,58 +1,49 @@
 package com.mystart.start.controller;
 
 import com.mystart.start.demo.UserProfile;
+import com.mystart.start.demo.mapper.UserProfileMapper;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 @RestController
 public class UserProfileController {
 
-    private Map<String, UserProfile> userMap;
+    private UserProfileMapper mapper;
 
-    @PostConstruct
-    public void init() {
-        userMap = new HashMap<String, UserProfile>();
-        userMap.put("1", new UserProfile("1", "홍길동", "111-1111", "서울시 강남구 대치1동"));
-        userMap.put("2", new UserProfile("2", "홍길자", "111-1112", "서울시 강남구 대치2동"));
-        userMap.put("3", new UserProfile("3", "홍길순", "111-1113", "서울시 강남구 대치3동"));
+    public UserProfileController(UserProfileMapper mapper) {
+        this.mapper = mapper;
     }
+
 
     /** 사용자 정보 id로 조회 */
     @GetMapping("/user/{id}")
     public UserProfile getUserProfile(@PathVariable("id") String id) {
-        return userMap.get(id);
+        return mapper.getUserProfile(id);
     }
 
     /** 사용자 정보 LIST 형태로 조회 */
     @GetMapping("/user/all")
     public List<UserProfile> getUserProfileList() {
-        return new ArrayList<UserProfile>(userMap.values());
+        return mapper.getUserProfileList();
     }
 
     /** 사용자 정보 생성 */
     @PutMapping("/user/{id}")
     public void putUserProfile(@PathVariable("id") String id, @RequestParam("name") String name, @RequestParam("phone") String phone, @RequestParam("address") String address) {
-        UserProfile userProfile = new UserProfile(id, name, phone, address);
-        userMap.put(id, userProfile);
+        mapper.insertUserProfile(id, name, phone, address);
     }
 
     /** 사용자 정보 수정 */
     @PostMapping("/user/{id}")
     public void postUserProfile(@PathVariable("id") String id, @RequestParam("name") String name, @RequestParam("phone") String phone, @RequestParam("address") String address) {
-        UserProfile userProfile = new UserProfile(id, name, phone, address);
-        userProfile.setName(name);
-        userProfile.setPhone(phone);
-        userProfile.setAddress(address);
+        mapper.updateUserProfile(id, name, phone, address);
     }
 
     /** 사용자 정보 삭제 */
     @DeleteMapping("/user/{id}")
     public void deleteUserProfile(@PathVariable("id") String id) {
-        userMap.remove(id);
+        mapper.deleteUserProfile(id);
     }
 }
